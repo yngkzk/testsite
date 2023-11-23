812066bcce55b04ad396e3c7e31df346d0f13d62
yngkzk <131941774+yngkzk@users.noreply.github.com>
2023-11-23 14:04:10 +0600

    Delete bboard directory

--- bboard/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import home_page, login_page
-
-urlpatterns = [
-    path('', home_page, name="index"),
-    path('home', home_page, name="home"),
-    path('login', login_page, name="login"),
-]
